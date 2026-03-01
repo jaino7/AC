07c09ad568edc7f987d8627c2d2c79b0a21e4ce2
myba <ownstage3m@gmail.com>
2026-03-01 09:53:35 +0900

    build成果物をGit管理から除外

--- apps/api/dist/creators/dto/create-creator.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateCreatorDto {
-    email: string;
-    password: string;
-    confirmPassword: string;
-    acceptTerms: boolean;
-    confirmAdult: boolean;
-}
--- apps/api/dist/creators/dto/login-creator.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class LoginCreatorDto {
-    email: string;
-    password: string;
-}
